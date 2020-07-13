218e611e86f31fc168dda6cf5ca5c9bf43db7bf4
Carlos Martin <carlos.martin@softhouse.se>
2020-07-13 10:17:20 +0200

    Initial project structure + Target configured

---
+++ AppFlavors-finished/View/BookmarksViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BookmarksViewController.swift
+//  AppFlavors-finished
+//
+//  Created by Carlos Martin on 2020-07-10.
+//  Copyright © 2020 Softhouse Nordic, AB. All rights reserved.
+//
+
+import UIKit
+
+class BookmarksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "Bookmarks"
+        self.view.backgroundColor = .systemGreen
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AppFlavors-finished/View/FavoritesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FavoritesViewController.swift
+//  AppFlavors-finished
+//
+//  Created by Carlos Martin on 2020-07-10.
+//  Copyright © 2020 Softhouse Nordic, AB. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "Favorites"
+        self.view.backgroundColor = .systemBlue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
